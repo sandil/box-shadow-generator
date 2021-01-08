9f79f0a38b187b6aed44e9b89b66485d1da23a2e
Rishikesh Sarje <rushi0241@gmail.com>
2021-01-08 13:30:46 +0530

    box-shadow-generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
 
 function App() {
+  const [Hori, setHori] = useState(10);
+  const [Verti, setVerti] = useState(10);
+  const [Blur, setBlur] = useState(10);
+  const [Color, setColor] = useState('black');
+  const [CheckOn, setCheckOn] = useState(false);
+  const [BoxBg, setBoxBg] = useState('chocolate');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><strong>Box Shadow Generator</strong></h1>
+      <div className="content">
+        <div className="controls">
+          <label>Horizontal Length</label>
+          <input type="range" min="-200" max="200" value={Hori} onChange={(e)=>setHori(e.target.value)} />
+          <label>Vertical Length</label>
+          <input type="range" min="-200" max="200" value={Verti} onChange={(e)=>setVerti(e.target.value)} />
+          <label>Blur</label>
+          <input type="range" min="0" max="200" value={Blur} onChange={(e)=>setBlur(e.target.value)} />
+          <label>Shadow Color</label>
+          <input className="color-pallet" type="color" value={Color} onChange={(e)=>setColor(e.target.value)} /><br/>
+          <label>Box Color</label>
+          <input className="color-pallet" type="color" value={BoxBg} onChange={(e)=>setBoxBg(e.target.value)} />
+          <div className="switch">
+            <label>
+              Outline
+              <input type="checkbox" value={CheckOn} onChange={()=>setCheckOn(!CheckOn)} />
+              <span className="lever"></span>
+              Inset
+            </label>
+          </div>
+        </div>
+        <div className="output" style={{backgroundColor: `${BoxBg}`, boxShadow: `${CheckOn ? 'inset': ''} ${Hori}px ${Verti}px ${Blur}px ${Color}`}}>
+          <p>
+            -webkit-box-shadow: {Hori}px {Verti}px {Blur}px {Color}
+          </p>
+          <p>
+            -moz-box-shadow: {Hori}px {Verti}px {Blur}px {Color}
+          </p>
+          <p>
+            box-shadow: {Hori}px {Verti}px {Blur}px {Color}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
